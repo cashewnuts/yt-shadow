f256d40b2b93e7893146878d91f83da8914d3793
Tatsuya Kanemoto <cashewnuts903@gmail.com>
2020-04-29 20:33:29 +0900

    Initial commit.

---
+++ yt-shadow/yt-shadow.js
@@ -0,0 +1,6 @@
+document.body.style.border = "5px solid red";
+console.log('hello')
+
+const ytVideo = document.querySelector("video[src*='blob:https://www.youtube.com']")
+window.ytVideo = ytVideo
+
