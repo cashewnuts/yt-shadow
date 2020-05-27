d42cbfd750d223f6838cf114b48046f5d00565fc
Tatsuya Kanemoto <cashewnuts903@gmail.com>
2020-05-27 13:59:33 +0900

    Add input

--- src/views/components/TranscriptWriter.tsx
+++ src/views/components/TranscriptWriter.tsx
@@ -1,13 +1,36 @@
-import React, { PropsWithChildren } from 'react'
+import React, { PropsWithChildren, CSSProperties } from 'react'
 import { SRTMeasure } from '../../models/srt'
 
 export interface TranscriptWriterProps {
   text: SRTMeasure
 }
 
+const styles: { [key: string]: CSSProperties } = {
+  inputContainer: {
+    height: '3em',
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  inputStyle: {
+    width: '80%',
+  },
+}
+
 const TranscriptWriter = (props: PropsWithChildren<TranscriptWriterProps>) => {
   const { text } = props
-  return <div>{text && <p>{text.text}</p>}</div>
+  return (
+    <div>
+      {text && (
+        <div>
+          <p>{text.text}</p>
+          <div style={styles.inputContainer}>
+            <input style={styles.inputStyle} />
+          </div>
+        </div>
+      )}
+    </div>
+  )
 }
 
 export default TranscriptWriter
