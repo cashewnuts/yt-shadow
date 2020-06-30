27f55efa7e9515ca739cf7a02e0ebdeaece3f2ae
Tatsuya Kanemoto <cashewnuts903@gmail.com>
2020-06-30 18:08:55 +0900

    Fix not to overwrite at bulkUpsert.

---
+++ README.md
@@ -0,0 +1,7 @@
+# Limitation
+
+This addon lacks browser suppurts for belows.
+
+- Internet Explorer
+- Edge(EdgeHTML)
+- Safari < v10
--- src/background.ts
+++ src/background.ts
@@ -10,6 +10,8 @@ import {
 } from './helpers/message-helper'
 import { db } from './storages/shadowing-db'
 import { createLogger } from './helpers/logger'
+import zip from 'lodash/zip'
+import { ITranscript } from './models/transcript'
 const logger = createLogger('background.ts')
 
 let portFromCS: browser.runtime.Port
@@ -20,9 +22,33 @@ async function databaseActionHandler(
 ) {
   if (instanceOfTranscriptBulkUpsertAction(action)) {
     let result = true
-    logger.debug('instanceOfTranscriptBulkUpsertAction')
     try {
-      await db.transcripts.bulkPut(action.value)
+      const keys = action.value.map(({ host, videoId, start }) => [
+        host,
+        videoId,
+        start,
+      ])
+      const savedTranscripts = await db.transcripts
+        .where('[host+videoId+start]')
+        .anyOf(keys)
+        .toArray()
+      const putItems: ITranscript[] = []
+      for (const [item, script] of zip(action.value, savedTranscripts)) {
+        if (!item) {
+          logger.error('bulkUpsert operation must be wrong', {
+            value: action.value,
+            savedTranscripts,
+          })
+          continue
+        }
+        // If different from db then put or overwrite
+        if (!script || item.text !== script.text) {
+          putItems.push(item)
+          continue
+        }
+      }
+      logger.debug('bulkPut', putItems)
+      await db.transcripts.bulkPut(putItems)
     } catch (err) {
       result = err
       logger.error(err)
@@ -37,7 +63,7 @@ async function databaseActionHandler(
     let result = null
     try {
       const { host, videoId } = action.value
-      logger.debug(host, videoId)
+      logger.debug('get', host, videoId)
       result = await db.transcripts.where({ host, videoId }).sortBy('start')
     } catch (err) {
       result = err
@@ -56,13 +82,16 @@ async function databaseActionHandler(
       const { host, videoId, start, ...rest } = value
       const transcript = await db.transcripts.get({ host, videoId, start })
       if (transcript) {
+        const { createdAt } = transcript
         const mergedObj = {
           ...transcript,
           ...rest,
+          createdAt,
+          updatedAt: Date.now(),
         }
         result = await db.transcripts.put(mergedObj)
       } else {
-        result = await db.transcripts.put(value)
+        result = await db.transcripts.put({ ...value, updatedAt: Date.now() })
       }
     } catch (err) {
       result = err
