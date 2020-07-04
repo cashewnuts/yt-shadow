be8ec850f55edb5162a1443383d766b6776873fc
Tatsuya Kanemoto <cashewnuts903@gmail.com>
2020-07-04 18:06:36 +0900

    Update make addon button can set active state.

---
+++ src/backgrounds/icon-path.ts
@@ -0,0 +1,10 @@
+export const iconPath = {
+  '24': 'icons/yt-24.png',
+  '48': 'icons/yt-48.png',
+  '96': 'icons/yt-96.png',
+}
+export const grayIconPath = {
+  '24': 'icons/yt-gray.svg',
+  '48': 'icons/yt-gray.svg',
+  '96': 'icons/yt-gray.svg',
+}
--- src/popup.ts
+++
@@ -1,1 +0,0 @@
-console.log('from popup.ts')
