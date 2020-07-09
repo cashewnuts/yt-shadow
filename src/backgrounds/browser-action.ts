5dea26ce80f32181c1ecdfff6f27f33adb3a21c7
Tatsuya Kanemoto <cashewnuts903@gmail.com>
2020-07-09 17:20:09 +0900

    Add popup and dashboard

--- src/backgrounds/browser-action.ts
+++ src/backgrounds/browser-action.ts
@@ -1,29 +1,39 @@
 import { iconPath, grayIconPath } from './icon-path'
 import { StorageKey, DefaultStorageObject } from '../storages/browser-storage'
+const { SETTING } = StorageKey
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
-export async function browserActionHandler(tab: browser.tabs.Tab) {
-  const { SETTING } = StorageKey
+export async function browserActionHandler(tab?: browser.tabs.Tab) {
   try {
-    const setting = await browser.storage.local.get({
-      [SETTING]: DefaultStorageObject[SETTING],
-    })
-    const { active } = setting[SETTING]
-    if (active) {
-      browser.browserAction.setIcon({
-        path: grayIconPath,
-      })
-    } else {
-      browser.browserAction.setIcon({
-        path: iconPath,
-      })
-    }
-    await browser.storage.local.set({
-      [SETTING]: {
-        active: !active,
-      },
-    })
+    await toggleActiveState()
   } catch (err) {
     console.error(err)
   }
+}
+
+export const toggleActiveState = async () => {
+  const { active } = await getCurrentSetting()
+  if (active) {
+    browser.browserAction.setIcon({
+      path: grayIconPath,
+    })
+  } else {
+    browser.browserAction.setIcon({
+      path: iconPath,
+    })
+  }
+  const activeState = !active
+  await browser.storage.local.set({
+    [SETTING]: {
+      active: activeState,
+    },
+  })
+  return activeState
+}
+
+export const getCurrentSetting = async () => {
+  const storageObj = await browser.storage.local.get({
+    [SETTING]: DefaultStorageObject[SETTING],
+  })
+  return storageObj[SETTING]
 }
