f3ba994a2ff568af5d5dbd2e4387b653bb72a045
tatsuya kanemoto <tatsuya.kanemoto@f.flect.co.jp>
2020-07-10 18:36:13 +0900

    Add videos table into indexedDB.

---
+++ src/models/video.ts
@@ -0,0 +1,52 @@
+import { VideoIndex } from '@/storages/shadowing-db'
+import { splitTextIntoWords } from '@/helpers/text-helper'
+
+export interface IVideo extends VideoIndex {
+  title: string
+  url: string
+  createdAt?: number
+  updatedAt?: number
+}
+
+export interface VideoParams extends IVideo {
+  title: string
+  words?: string[]
+}
+
+export default class Video implements IVideo {
+  host: string
+  videoId: string
+  words: string[]
+  title: string
+  url: string
+  dur?: number
+  createdAt: number
+  updatedAt: number
+  constructor({
+    host,
+    videoId,
+    words,
+    title,
+    url,
+    createdAt,
+    updatedAt,
+  }: VideoParams) {
+    this.host = host
+    this.videoId = videoId
+    this.title = title
+    this.url = url
+    this.words =
+      words ||
+      Object.keys(
+        splitTextIntoWords(title).reduce(
+          (wordObj: { [key: string]: boolean }, word) => {
+            wordObj[word] = true
+            return wordObj
+          },
+          {}
+        )
+      )
+    this.createdAt = createdAt || Date.now()
+    this.updatedAt = updatedAt || Date.now()
+  }
+}
--- src/storages/shadowing-db.ts
+++ src/storages/shadowing-db.ts
@@ -1,17 +1,21 @@
 import Dexie from 'dexie'
 import { ITranscript } from '@/models/transcript'
+import { IVideo } from '@/models/video'
 
 export default class ShadowingDatabase extends Dexie {
   transcripts: Dexie.Table<ITranscript, TranscriptIndex>
+  videos: Dexie.Table<IVideo, VideoIndex>
 
   constructor() {
     super('ShadowingDatabase')
     this.version(1).stores({
       transcripts: '[host+videoId+start], [host+videoId], *words',
+      videos: '[host+videoId], host, *words',
     })
     // The following line is needed if your typescript
     // is compiled using babel instead of tsc:
     this.transcripts = this.table('transcripts')
+    this.videos = this.table('videos')
   }
 }
 
@@ -19,6 +23,11 @@ export interface TranscriptIndex {
   host: string
   videoId: string
   start: number
+  words?: string[]
+}
+export interface VideoIndex {
+  host: string
+  videoId: string
   words?: string[]
 }
 
